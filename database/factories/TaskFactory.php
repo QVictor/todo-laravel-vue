862ae6e899568acf41ba522984cd19ccd2533b97
QVictor <QQlogin@yandex.ru>
2021-12-08 16:13:20 +0300

    seeder(task): + seeder for task table

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->realText(20),
+            'completed' => $this->faker->numberBetween(0,1),
+            'completed_at' => $this->faker->dateTimeBetween(
+        Carbon::today()->format('Y-m-d H:m:s'),
+        Carbon::today()->addMonths(6)->format('Y-m-d H:m:s')),
+        ];
+    }
+}
--- database/migrations/2021_11_30_112738_create_tasks_table.php
+++ database/migrations/2021_11_30_112738_create_tasks_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use Database\Seeders\TaskSeeder;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -20,6 +21,7 @@ class CreateTasksTable extends Migration
             $table->timestamp('completed_at')->nullable();
             $table->timestamps();
         });
+        (new TaskSeeder())->run();
     }
 
     /**
